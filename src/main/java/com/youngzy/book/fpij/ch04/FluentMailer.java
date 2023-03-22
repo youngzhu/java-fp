42bee6061d4f6072eaa6726b50f6d66d5951bd7f
young-win <young.zhu.2012@gmail.com>
2023-03-22 14:12:06 +0800

    Chapter 4: Designing with Lambda Expressions: Method Chaining

---
+++ src/main/java/com/youngzy/book/fpij/ch04/FluentMailer.java
@@ -0,0 +1,42 @@
+package com.youngzy.book.fpij.ch04;
+
+import java.util.function.Consumer;
+
+/**
+ * @author youngzy
+ * @since 2023-03-21
+ */
+public class FluentMailer {
+    private FluentMailer() {
+    }
+
+    public FluentMailer from(final String address) {
+        return this;
+    }
+
+    public FluentMailer to(final String address) {
+        return this;
+    }
+
+    public FluentMailer subject(final String subject) {
+        return this;
+    }
+
+    public FluentMailer body(final String message) {
+        return this;
+    }
+
+    public static void send(Consumer<FluentMailer> block) {
+        FluentMailer mailer = new FluentMailer();
+        block.accept(mailer);
+        System.out.println("send...");
+    }
+
+    public static void main(String[] args) {
+        FluentMailer.send(mailer ->
+                mailer.from("from@youngzy.com")
+                        .to("to@youngzy.com")
+                        .subject("Say hi")
+                        .body("Hello, "));
+    }
+}
