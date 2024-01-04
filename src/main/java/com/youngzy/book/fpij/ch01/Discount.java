e49c01679c1d3effa828c8feb81d1340dbea25b8
young-win <young.zhu.2012@gmail.com>
2024-01-04 13:41:40 +0800

    Ch01 Hello, Lambda Expressions!

--- src/main/java/com/youngzy/book/fpij/ch01/Discount.java
+++ src/main/java/com/youngzy/book/fpij/ch01/Discount.java
@@ -1,46 +1,38 @@
 package com.youngzy.book.fpij.ch01;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
 /**
  * @author youngzy
- * @since 2023-02-16
+ * @since 2024-01-04
  */
 public class Discount {
-    public static void main(String[] args) {
-        List<BigDecimal> prices = Arrays.asList(
-                new BigDecimal("10"), new BigDecimal("30"),
-                new BigDecimal("17"), new BigDecimal("20"),
-                new BigDecimal("15"), new BigDecimal("18"),
-                new BigDecimal("45"), new BigDecimal("12")
-        );
+    static final List<Integer> prices = Arrays.asList(10, 30, 17, 20, 18, 45, 12);
 
-        discountImperative(prices);
-        discountFunctional(prices);
-    }
-
-    private static void discountFunctional(List<BigDecimal> prices) {
-        BigDecimal totalOfDiscountedPrices = prices.stream()
-                .filter(price -> price.compareTo(BigDecimal.valueOf(20)) > 0)
-                .map(price -> price.multiply(BigDecimal.valueOf(0.9)))
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    private static void discountFunctional(List<Integer> prices) {
+        final double totalOfDiscountedPrices = prices.stream()
+                .filter(price -> price > 20)
+                .mapToDouble(price -> price * 0.9)
+                .sum();
 
         System.out.println("Total of discounted prices: " + totalOfDiscountedPrices);
     }
 
-    private static void discountImperative(List<BigDecimal> prices) {
-        BigDecimal totalOfDiscountedPrices = BigDecimal.ZERO;
+    private static void discountImperative(List<Integer> prices) {
+        double totalOfDiscountedPrices = 0.0;
 
-        for (BigDecimal price : prices) {
-            if (price.compareTo(BigDecimal.valueOf(20)) > 0) {
-                totalOfDiscountedPrices = totalOfDiscountedPrices.add(
-                        price.multiply(BigDecimal.valueOf(0.9))
-                );
+        for (int price : prices) {
+            if (price > 20) {
+                totalOfDiscountedPrices += price * 0.9;
             }
         }
 
         System.out.println("Total of discounted prices: " + totalOfDiscountedPrices);
+    }
+
+    public static void main(String[] args) {
+        discountImperative(prices);
+        discountFunctional(prices);
     }
 }
