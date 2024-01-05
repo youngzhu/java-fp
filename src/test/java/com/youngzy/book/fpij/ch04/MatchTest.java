5d129a3348622f4489a06001df2252e284fa9d18
young-win <young.zhu.2012@gmail.com>
2024-01-05 17:14:16 +0800

    Ch04, anyMatch, allMatch, noneMatch

---
+++ src/test/java/com/youngzy/book/fpij/ch04/MatchTest.java
@@ -0,0 +1,36 @@
+package com.youngzy.book.fpij.ch04;
+
+import org.junit.Test;
+
+import static com.youngzy.book.fpij.ch04.Person.SAMPLE_DATA;
+import static org.junit.Assert.*;
+
+public class MatchTest {
+
+    @Test
+    public void anyMatch() {
+        assertTrue(SAMPLE_DATA.stream()
+                .anyMatch(person -> person.emailAddresses().size() > 0));
+
+        assertFalse(SAMPLE_DATA.stream()
+                .anyMatch(person -> person.emailAddresses().size() > 10));
+    }
+
+    @Test
+    public void allMatch() {
+        assertFalse(SAMPLE_DATA.stream()
+                .allMatch(person -> person.emailAddresses().size() > 0));
+
+        assertTrue(SAMPLE_DATA.stream()
+                .anyMatch(person -> person.emailAddresses().size() >= 0));
+    }
+
+    @Test
+    public void noneMatch() {
+        assertFalse(SAMPLE_DATA.stream()
+                .noneMatch(person -> person.emailAddresses().size() > 0));
+
+        assertTrue(SAMPLE_DATA.stream()
+                .noneMatch(person -> person.emailAddresses().size() > 10));
+    }
+}
